b2e0490629c2353b41587efa83531cd7b02dedb3
Anthony Vega <tonynorte@gmail.com>
2022-05-27 20:42:38 -0600

    mixng para el manejo de espaciados, react-bootsrap, router

---
+++ src/Assets/Config/images.js
@@ -0,0 +1,196 @@
+const images = {
+  img1: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/1.jpg')
+  },
+  img2: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/2.jpg')
+  },
+  img3: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/3.jpg')
+  },
+  img4: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/4.jpg')
+  },
+  img5: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/5.jpg')
+  },
+  img6: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/6.jpg')
+  },
+  img7: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/7.jpg')
+  },
+  img8: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/8.jpg')
+  },
+  img9: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/9.jpg')
+  },
+  img10: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/10.jpg')
+  },
+  img11: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/11.jpg')
+  },
+  img12: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/12.jpg')
+  },
+  img13: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/13.jpg')
+  },
+  img14: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/14.jpg')
+  },
+  img15: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/15.jpg')
+  },
+  img16: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/16.jpg')
+  },
+  img17: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/17.jpg')
+  },
+  img18: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/18.jpg')
+  },
+  img19: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/19.jpg')
+  },
+  img20: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/20.jpg')
+  },
+  img21: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/21.jpg')
+  },
+  img22: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/22.jpg')
+  },
+  img23: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/23.jpg')
+  },
+  img24: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/24.jpg')
+  },
+  img25: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/25.jpg')
+  },
+  img26: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/26.jpg')
+  },
+  img27: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/27.jpg')
+  },
+  img28: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/28.jpg')
+  },
+  img29: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/29.jpg')
+  },
+  img30: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/30.jpg')
+  },
+  img31: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/31.jpg')
+  },
+  img32: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/32.jpg')
+  },
+  img33: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/33.jpg')
+  },
+  img34: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/34.jpg')
+  },
+  img35: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/35.jpg')
+  },
+  img36: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/36.jpg')
+  },
+  img37: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/37.jpg')
+  },
+  img38: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/38.jpg')
+  },
+  img39: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/39.jpg')
+  },
+  img40: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/40.jpg')
+  },
+  img41: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/41.jpg')
+  },
+  img42: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/42.jpg')
+  },
+  img43: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/43.jpg')
+  },
+  img44: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/44.jpg')
+  },
+  img45: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/45.jpg')
+  },
+  img46: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/46.jpg')
+  },
+  img47: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/47.jpg')
+  },
+  img48: {
+    type: 'postman',
+    portfolioImage: require('../../Assets/Img/48.jpg')
+  },
+};
+
+export default images;
